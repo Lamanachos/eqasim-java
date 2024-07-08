8cc2ab81564ebc5028a7ca62a09ba937d9c990e7
Lamanachos <32.ul.mar@gmail.com>
2024-07-08 17:55:47 +0200

    density

--- python_files/get_data/area.py
+++ python_files/get_data/area.py
@@ -10,6 +10,14 @@ for i in gdf.iterrows():
     insee = i[1].insee
     if insee not in dict_data.keys() :
         dict_data[insee] = {}
-    dict_data[insee]["area"] = area
+    dict_data[insee]["area"] = area/1000000
 file_pop = "python_files\\get_data\donnees-communales-sur-la-population-dile-de-france.csv"
-
+df = pd.read_csv(file_pop,sep=";")
+for i in df.iterrows() :
+    pop = i[1].popmun2017
+    insee = i[1].insee
+    if insee not in dict_data.keys() :
+        dict_data[insee] = {}
+    dict_data[insee]["pop"] = pop
+    dict_data[insee]["density"] = pop/dict_data[insee]["area"]
+print(dict_data)
