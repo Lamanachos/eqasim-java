cfb68337789c03043546fd32e5ec3f4198084072
Lamanachos <32.ul.mar@gmail.com>
2024-07-25 14:53:41 +0200

    o

---
+++ python_files/get_data/reset_shapefile.py
@@ -0,0 +1,18 @@
+import geopandas as gpd
+import os 
+
+shapefile_communes = "python_files\\communes-dile-de-france-au-01-janvier\\communes-dile-de-france-au-01-janvier.shp"
+shapefile_fusions = "python_files\\communes-dile-de-france-au-01-janvier\\communes-dile-de-france-au-01-janvier-with-fusions.shp"
+all_insee_files = "python_files\\communes-dile-de-france-au-01-janvier\\existing-insees.txt"
+gis_folder = "gis"
+
+f = open(all_insee_files,"w")
+f.close()
+gdf = gpd.read_file(shapefile_communes)
+gdf.to_file(shapefile_fusions)
+list_dir = os.listdir(gis_folder)
+for directory in list_dir :
+    if os.path.isdir(gis_folder + "\\" + directory):
+        for i in os.listdir(gis_folder + "\\" + directory):
+            os.remove(gis_folder + "\\" + directory + "\\" + i)
+        os.rmdir(gis_folder + "\\" + directory)
