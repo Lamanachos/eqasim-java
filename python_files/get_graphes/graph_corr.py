5eb8da5d67753d3aa994e60d250a3bdc47c1abea
Lamanachos <32.ul.mar@gmail.com>
2024-09-16 11:15:12 +0200

    graph_corr_ok

--- python_files/get_graphes/graph_corr.py
+++ python_files/get_graphes/graph_corr.py
@@ -2,20 +2,33 @@ import attributes as attrib
 import matplotlib.pyplot as plt
 from math import ceil
 
-df_data = attrib.get_data()
-df_results = attrib.get_results()
+def make_graph_corr(y_name = "er_idf"):
+    plt.clf()
+    df_data = attrib.get_data()
+    df_results = attrib.get_results()
 
-y_name = "er_idf"
-Y = df_results[y_name]
+    Y = df_results[y_name]
 
-nb = len(df_data.columns)
-liste_col = list(df_data.columns)
-for i in range(nb):
-    plt.subplot(3, ceil(nb/3),i+1)
-    col_name = liste_col[i]
-    X = df_data[col_name]
-    plt.scatter(X,Y)
-    plt.xlabel(col_name)
-    plt.ylabel(y_name)
-plt.show()
+    plt.rc('xtick', labelsize=6) 
+    plt.rc('ytick', labelsize=6) 
+
+    nb = len(df_data.columns) - 1
+    liste_col = list(df_data.columns)
+    ind = 1
+    for col in liste_col:
+        if col != "insee":
+            plt.subplot(3, ceil(nb/3),ind)
+            X = df_data[col]
+            plt.scatter(X,Y)
+            plt.xlabel(col, fontsize = 6)
+            plt.ylabel(y_name, fontsize = 6)
+            ind += 1
+    figure = plt.gcf()
+    figure.set_size_inches(11.7,8.3)
+    plt.savefig(attrib.graphes_folder+"\\graph_corr_"+y_name,dpi = 300)
+
+y_names = ["car_ms_res_nb","car_ms_inout_nb","car_ms_idf_nb","att_res","att_inout","att_idf","er_0","er_10","er_20","er_idf"]
+for y_name in y_names :
+    make_graph_corr(y_name)
+
 
